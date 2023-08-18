41c2470f22890b869557d486d787ace023ff225c
karan <karanh123@gmail.com>
2023-08-18 12:42:38 +0530

    lambda

---
+++ p7l.py
@@ -0,0 +1,15 @@
+#lambda
+a = lambda x:x+5
+print(a(2))
+
+b = lambda x,y : x+y
+print(b(5,10))
+
+c = lambda a,b : a if (a > b) else b
+print(c(6,3))
+
+d = lambda a : a*a
+print(d(5))
+print()
+
+
